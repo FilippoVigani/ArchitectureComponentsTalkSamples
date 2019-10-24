d09950566397697067017b7c9662db644f546f4e
Filippo Vigani <vigani.filippo@gmail.com>
2019-10-24 22:31:36 +0200

    Move counter update logic to ViewModel

--- app/src/main/java/com/example/acts/counter/CounterViewModel.kt
+++ app/src/main/java/com/example/acts/counter/CounterViewModel.kt
@@ -4,4 +4,12 @@ import androidx.lifecycle.ViewModel
 
 class CounterViewModel : ViewModel() {
     var counter = 0
+
+    fun increaseCounter(){
+        counter++
+    }
+
+    fun decreaseCounter(){
+        counter--
+    }
 }
