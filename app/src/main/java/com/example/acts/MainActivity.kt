78d07bd92826802c925f082c6e87f2b81da4c2bb
Filippo Vigani <vigani.filippo@gmail.com>
2019-11-05 00:28:41 +0100

    Add scaffold navigation

--- app/src/main/java/com/example/acts/MainActivity.kt
+++ app/src/main/java/com/example/acts/MainActivity.kt
@@ -2,22 +2,28 @@ package com.example.acts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.commit
-import com.example.acts.counter.CounterFragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mainFragment: CounterFragment by lazy { CounterFragment() }
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(savedInstanceState == null){
-            supportFragmentManager.commit {
-                add(R.id.mainView, mainFragment)
-            }
-        }
+        setSupportActionBar(toolbar)
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
